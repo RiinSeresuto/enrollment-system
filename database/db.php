aff249fb5f6d26b3504ebcd5645d5bb778665276
Lester <renebaribal@gmail.com>
2023-01-30 21:51:23 +0800

    initial commit, boilerplate created

---
+++ database/db.php
@@ -0,0 +1,16 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'enrolment-system');
+
+// Connecting to server
+$server = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
+
+if (!$server) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+  echo "Server Connected";
+}
+?>
---
+++ views/Home.php
@@ -0,0 +1,14 @@
+<?php
+require '.\database\db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+    <?php echo "Home"; ?>
+  </body>
+</html>
