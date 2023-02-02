23fcdae3145f4741a36bf907744759c16205ba5e
Lester <renebaribal@gmail.com>
2023-02-02 21:47:56 +0800

    finished users lists template

---
+++ views/UsersLists.php
@@ -0,0 +1,37 @@
+<article class="table-card">
+  <div class="table-title">
+    Faculty Members
+  </div>
+  <div class="table-body">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Position</th>
+          <th>Grade Level</th>
+          <th>Contact Number</th>
+          <th>Address</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Sam Sun</td>
+          <td>Master Teacher VI</td>
+          <td>13</td>
+          <td>09951753654</td>
+          <td>Biringan City</td>
+          <td>O</td>
+        </tr>
+        <tr>
+          <td>Sam Sun</td>
+          <td>Master Teacher VI</td>
+          <td>13</td>
+          <td>09951753654</td>
+          <td>Biringan City</td>
+          <td>O</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</article>
